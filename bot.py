3d45f67a7f05480abf22943664a7619fc5a8f1e1
Göktuğ Aşcı <g.asci@hotmail.com>
2022-01-25 15:36:14 +0100

    first commit

--- bot.py
+++ bot.py
@@ -2,19 +2,56 @@ from dotenv import load_dotenv
 import os
 import telebot
 
-# load all environments
-load_dotenv()
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
-TELEGRAM_API_KEY = os.environ.get("TELEGRAM_API_KEY")
 
-bot = telebot.TeleBot(TELEGRAM_API_KEY)
+class HouseBot:
 
-@bot.message_handler(commands=["answer"])
-def greet(message):
-    bot.reply_to(message, "Yes, I am answering.")
+    def __init__(self):
 
-@bot.message_handler(commands=["greet me"])
-def greet(message):
-    bot.send_message(message.chat.id, "Hey! Hows it going?")
+        # load all environments
+        load_dotenv()
 
-bot.polling()
+        self.TELEGRAM_API_KEY = os.environ.get("TELEGRAM_API_KEY")
+        self.bot = telebot.TeleBot(self.TELEGRAM_API_KEY)
+        
+
+        # Handle /start command
+        @self.bot.message_handler(commands=["start"])
+        def start_command(message):
+            data = self.fetch_data()
+
+            result = data["result"]
+            
+            # run the function once
+            self.reply_to_bot(message, result)
+
+            # run it with an interval
+            self.scheduler = BackgroundScheduler(timezone="Europe/Berlin")
+            self.scheduler.add_job(func=lambda: self.reply_to_bot(message, result), trigger="interval", seconds=1) #60 * 60)
+            self.scheduler.start()
+
+            atexit.register(lambda: self.scheduler.shutdown())
+
+
+        # Handle /start command
+        @self.bot.message_handler(commands=["stop"])
+        def stop_command(message):
+            self.scheduler.shutdown()
+            
+        self.bot.polling()
+
+    def reply_to_bot(self, message, output):
+        self.bot.reply_to(message, output)
+
+    def fetch_data(self):
+
+        data = {}
+        data["result"] = 5 + 5
+
+        return data
+
+house_bot = HouseBot()
+
+
